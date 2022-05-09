24648821cf8631346e4e466bdee2b09d7f7a85ad
LeeInGyu <developlee20@gmail.com>
2022-05-09 20:35:04 +0900

    2022-05-09 version 1.1.1

--- Main.py
+++ Main.py
@@ -10,5 +10,6 @@ bot = pika.getting_started()
 @bot.command(aliases=['안녕'])
 async def hi(ctx):
     await ctx.send(f'안녕하세요 {ctx.author.mention} 님')
+    print(f'{ctx.author} used hi.')
 
 bot.run(token.get_token())
